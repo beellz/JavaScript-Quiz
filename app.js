3f8c131dc35139b2d974a652b21da4a0229d959c
beellz <belal20795@gmail.com>
2019-09-02 17:36:19 +0530

    created main frame

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert ("JS connected");
+function gameon() {
+    const move = document.getElementById("move-off");
+    
+   
+
+    const quizContainer = document.getElementById("quizContainer").style.display = 'none';
+    move.addEventListener("click", start);
+    
+    function start () {
+        move.style.display = 'none';
+        document.getElementById("quizContainer").style.display  = 'block';
+       
+    }
+
+}
+
+gameon();
